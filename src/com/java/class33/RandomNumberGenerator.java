05175b03f8c76dbea525bac6f764235f6f0487dd
Andrii Parashchuk <andrii.parashchuk89@gmail.com>
2022-12-01 21:48:38 -0600

    class33

---
+++ src/com/java/class33/RandomNumberGenerator.java
@@ -0,0 +1,11 @@
+package com.java.class33;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(74));
+
+    }
+}
---
+++ src/com/java/practice/encapsulation/Bank.java
@@ -0,0 +1,15 @@
+package com.java.practice.encapsulation;
+
+public class Bank {
+    private int balance;
+    private String accName;
+    private int accNumber;
+
+    public int getBalance(){
+        return balance;
+    }
+    public void setBalance(int balance){
+        this.balance=balance;
+
+    }
+}
